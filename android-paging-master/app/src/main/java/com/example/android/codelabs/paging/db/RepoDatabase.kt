140335290127464492153a8cce8115a2ce8e1f37
mtnmr <mei.rila1013@gmail.com>
2022-04-08 13:08:12 +0900

    Room RepoDatabase

---
+++ android-paging-master/app/src/main/java/com/example/android/codelabs/paging/db/RepoDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.android.codelabs.paging.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.android.codelabs.paging.model.Repo
+
+
+@Database(entities = [Repo::class], version = 1, exportSchema = false)
+abstract class RepoDatabase: RoomDatabase() {
+    abstract fun reposDao():RepoDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE:RoomDatabase ?= null
+
+        fun getInstance(context: Context): RoomDatabase =
+            INSTANCE ?: synchronized(this){
+                INSTANCE
+                    ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context):RoomDatabase =
+            Room.databaseBuilder(context.applicationContext,
+                RepoDatabase::class.java, "GitHub.db")
+                .build()
+    }
+}
